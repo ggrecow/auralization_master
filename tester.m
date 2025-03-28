b453c291de39db25d2eae76abcbb39bfe7054af9
Gil Felix Greco <67735211+ggrecow@users.noreply.github.com>
2025-03-28 16:11:41 +0100

    Update tester.m

--- tester.m
+++ tester.m
@@ -13,7 +13,7 @@ clear all; close all; clc;
 clear all; close all; clc;
 
 % input data
-core_path = [pwd '\TEST_input_data'];
+core_path = [pwd '\input_data'];
 
 % input_file
 input_file = [ core_path '\input_file_auralization.ini' ] ;
