bcdbf0621e4f61f5255b6b4352844a374b2a09ad
Gil Felix Greco <67735211+ggrecow@users.noreply.github.com>
2025-04-02 18:53:15 +0200

    Update overlapp_add_convolution.m
    included option for dealing with stereo output files, necessary for binarual rendering

--- auralization/private/overlapp_add_convolution.m
+++ auralization/private/overlapp_add_convolution.m
@@ -1,5 +1,5 @@
-function outputSignal = overlapp_add_convolution( inputSignal, BlockLen, IR )
-% function outputSignal = overlapp_add_convolution( inputSignal, BlockLen, IR )
+function outputSignal = overlapp_add_convolution( inputSignal, BlockLen, IR, type )
+% function outputSignal = overlapp_add_convolution( inputSignal, BlockLen, IR, type)
 %
 %   perform FFT-based block convolution using the overlap and add technique
 %
@@ -12,6 +12,9 @@ function outputSignal = overlapp_add_convolution( inputSignal, BlockLen, IR )
 %   IR : vector
 %   impulse response, with size <nTaps>
 %
+%   type  string
+%   'mono' or 'stereo'
+%
 %   output:
 %   outputSignal : vector
 %   convoluted output signal
@@ -19,22 +22,50 @@ function outputSignal = overlapp_add_convolution( inputSignal, BlockLen, IR )
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % get number of Blocks for convolution
-L = ceil(size(inputSignal,1)/BlockLen);
+L = floor(size(inputSignal,1)/BlockLen);
 
 % zero Pad input signal to integer divisor of block size N
 inputSignal(end+1:BlockLen*L,:) = 0;
 
-% get size of the FIR filter
-nTaps = length(IR);
+switch type
+    case 'mono'
 
-% allocate output signal
-outputSignal = zeros(BlockLen*L+nTaps-1, 1);
+        % get size of the FIR filter
+        nTaps = length(IR);
 
-% fft filt using overlap and add
-for ll = 1:L
+        % allocate output signal
+        outputSignal = zeros(BlockLen*L+nTaps-1, 1);
 
-    outputSignal((ll-1)*BlockLen+1:ll*BlockLen+nTaps-1,:) = ...
-                                                                                            outputSignal((ll-1)*BlockLen+1:ll*BlockLen+nTaps-1,:) ...
-                                                                                            + fftfilt(inputSignal((ll-1)*BlockLen+1:ll*BlockLen,:), [IR(:,ll); zeros(BlockLen-1, 1)]);
+        % fft filt using overlap and add
+        for ll = 1:L
+
+            outputSignal((ll-1)*BlockLen+1:ll*BlockLen+nTaps-1,:) = ...
+                                                                                                    outputSignal((ll-1)*BlockLen+1:ll*BlockLen+nTaps-1,:) ...
+                                                                                                    + fftfilt(inputSignal((ll-1)*BlockLen+1:ll*BlockLen,:), [IR(:,ll); zeros(BlockLen-1, 1)]);
+
+        end
+
+    case 'stereo'
+
+        % get size of the FIR filter
+        nTaps = size(IR.l_a,1);
+
+        % allocate output signal
+        outputSignal = zeros(BlockLen*L+nTaps-1, 2);
+
+        % fft filt using overlap and add
+        for ll = 1:L
+
+            outputSignal((ll-1)*BlockLen+1:ll*BlockLen+nTaps-1,:) = ...
+                                                                                                    outputSignal((ll-1)*BlockLen+1:ll*BlockLen+nTaps-1,:) ...
+                                                                                                    + fftfilt(inputSignal((ll-1)*BlockLen+1:ll*BlockLen,:), [[IR.l_a(:,ll) IR.r_a(:,ll)]; zeros(BlockLen-1, 2)]);
+
+        end
+
 
 end
+
+
+
+
+
