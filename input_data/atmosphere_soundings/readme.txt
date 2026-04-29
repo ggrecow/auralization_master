d4addc7f5bba53fe6a739816b435945d3960a993
Gil Felix Greco <ggrecow@gmail.com>
2026-04-29 14:40:43 +0200

    improving documentation regarding .ini files and atmospheric soundings

--- input_data/atmosphere_soundings/readme.txt
+++ input_data/atmosphere_soundings/readme.txt
@@ -12,3 +12,8 @@ These files were used to conduct the study on Chapter 4 of the following work:
 
 G. Felix Greco, Sound quality analysis of virtual aircraft prototypes: framework development and application. 
 Doctoral thesis, Technische Universität Braunschweig, 2026. DOI: 10.24355/dbbs.084-202512262308-0
+
+
+--
+
+log: created 29.04.2026 by Gil Felix Greco
--- run_auralization.m
+++ run_auralization.m
@@ -1,14 +1,14 @@
-clear all; close all; clc;
 % script run_auralization.m
 %
-%   Gives an example on how to call the auralization framework
+% Gives an example on how to call the auralization framework
 %
-%   - Input data: generic flyover of an A319 during approach procedure
-%   (therefore, there are no (buzzsaw and fan) tonal components from the engine noise) 
+% INPUT DATA:
 %
-%   - Only one receiver, positioned at (x,y,z) = (0, 0, 1.2) meters
+%   - exemplary case available under the '\input_data' folder 
 %
-%   INPUT DATA:
+%   - Generic flyover of an A319 during approach procedure
+%
+%   - has only one receiver, positioned at (x,y,z) = (0, 0, 1.2) meters
 %
 %   auralization_input.dat - sound emissions per time step, per sound
 %   source. File needs to have this name.
@@ -20,13 +20,19 @@ clear all; close all; clc;
 %   related to signal processing and setup of atmospherical conditions used
 %   for sound propagation simulation 
 %
-% Assumption: all required inputs are available under the '\input_data' folder 
+%   input_file_auralization_import_atmosphere.ini - initializing file, same
+%   as above, but is provided as an extra example to show how to import 
+%   atmospheric soundings as .txt file from 
+%   http://weather.uwyo.edu/upperair/sounding_legacy.html
+%
+% Assumption: all required inputs are available  
+%
 % -------------------------------
 % Author: Gil Felix Greco (ggrecow@gmail.com)
 % Institution: Technische Universität Braunschweig 
 %
 % Date created: 13.03.2025
-% Date last modified: 19.09.2025
+% Date last modified: 29.04.2026
 % MATLAB version: 2024b
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clear all; close all; clc;
@@ -43,31 +49,34 @@ input_file = [ core_path '\input_file_auralization.ini' ] ;
 % All output files (plots, data) are also saved using this tag in their filenames.
 tag = 'VR_approach';  
 
-%% call auralization_master(core_path)
+%% Ex. 1 - call auralization_master(core_path)
 
-% Ex. 1: only <core_path> is provided. Then, tag = 'auralization_results', 
+% Only <core_path> is provided. Then, tag = 'auralization_results', 
 % and the code looks for an .ini inside <core_path>. Results are stored at <core_path> 
+
+% WARNING: this setup only works if you have only one .ini file inside <core_path>
 
 % auralization_master(core_path);
 
-%% call auralization_master(core_path, tag)
+%% Ex. 2 - call auralization_master(core_path, tag)
 
-% Ex. 2: <core_path> and <tag> are provided. The 
-% code will look for an 
+% Here, <core_path> and <tag> are provided. The code will look for an 
 % input_file inside <core_path>. Results are stored at <core_path> 
+
+% WARNING: this setup only works if you have only one .ini file inside <core_path>
 
 % auralization_master(core_path, tag); 
 
-%% call auralization_master(core_path, tag, input_file)
+%% Ex. 3 - call auralization_master(core_path, tag, input_file)
 
-% Ex. 3: <core_path>, <tag>, and <input_file> are provided. 
+% Here, <core_path>, <tag>, and <input_file> are provided. 
 % Results are stored at <core_path> 
 
 % auralization_master(core_path, tag, input_file); 
 
-%% call auralization_master(core_path, tag, input_file, results_path)
+%% Ex. 4 -call auralization_master(core_path, tag, input_file, results_path)
 
-% Ex. 4: all inputs are provided. Results are stored at <results_path>. 
+% In this case, all inputs are provided. Results are stored at <results_path>. 
 % HINT: <results_path> will be automatically created if non existent  
 
 results_path = [pwd '\output_data_test\'];
